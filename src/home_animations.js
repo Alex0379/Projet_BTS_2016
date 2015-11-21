2897fbacbf1290238b1b7eebe0e3bf25aaf6630a
Alex0379 <alex.pacaud.79@gmail.com>
2015-11-21 13:12:31 +0100

    Modification des couleurs
    Modification des couleurs + animations

--- src/home_animations.js
+++ src/home_animations.js
@@ -6,12 +6,12 @@ jQuery(document).ready(function() {
     });
 
     // Annimation sur la barre de recherche
-    $('#barreRecherche').bind("focus",function() {
+    $('#barreRecherche').bind('focus',function() {
             $(this).css('box-shadow', '1px 1px 12px #555');
-            $(this).parent().css("background-color", "rgba(192, 180, 203, 0.7");
-        }).bind("focusout",function() {
-        $(this).css('box-shadow', '0px 0px 0px #555');
-        $(this).parent().css("background-color", "#795A99");
+            $(this).parent().css('background-color', 'rgba(0, 173, 238, 0.7)');
+        }).bind('blur',function() {
+        $(this).css('box-shadow', '');
+        $(this).parent().css('background-color', '');
         })
 
 });
