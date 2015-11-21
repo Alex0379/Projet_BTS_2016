4885066096bfa2a9e5d3153c5d33739db8e1ba55
Alex0379 <alex.pacaud.79@gmail.com>
2015-11-21 12:34:33 +0100

    Changement de la barre de navigation
    Réorganisation de la barre de navigation + Ajout d'animation en jQ

--- src/home_animations.js
+++ src/home_animations.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function() {
 
+    // Afficher/Masquer le Menu de navigation
     $('#boutonMenu').click(function() {
         $('#navMenu').slideToggle();
-    })
+    });
+
+    // Annimation sur la barre de recherche
+    $('#barreRecherche').bind("focus",function() {
+            $(this).css('box-shadow', '1px 1px 12px #555');
+            $(this).parent().css("background-color", "rgba(192, 180, 203, 0.7");
+        }).bind("focusout",function() {
+        $(this).css('box-shadow', '0px 0px 0px #555');
+        $(this).parent().css("background-color", "#795A99");
+        })
+
 });
